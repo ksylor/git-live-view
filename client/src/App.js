0366b5b76300d8f8c999f7ee03ad49c5e32cb757
Katie Sylor-Miller <ksylormiller@etsy.com>
2018-10-05 22:26:26 -0400

    It works

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = {
+        response: ''
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
+
+    render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+            {this.state.response}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,28 @@
+const nodegit = require("nodegit");
+const path = require("path");
+
+const pathToRepo = path.resolve("../ohshitgit");
+let walker = null;
+// first we open the repo in order to interact with it
+nodegit.Repository.open(pathToRepo)
+    .then(function(repo) {
+        // create the walker and save it for later
+        walker = nodegit.Revwalk.create(repo);
+        console.log(repo.path());
+        // get the commit at HEAD so we can work backwards from that
+        return repo.getHeadCommit();
+    }).then(function(commit) {
+        // walk back down the tree from the head commit backwards
+        console.log("head: " + commit.sha());
+        walker.sorting(nodegit.Revwalk.SORT.TIME);
+        walker.push(commit.id());
+        return walker.getCommits(10)
+    }).then(function (commits) {
+        // note the head commit is repeated in the list, we can skip it
+        // but for now just keep it
+        commits.forEach(function(commit) {
+           console.log(commit.sha());
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
